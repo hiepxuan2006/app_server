ba0348b1f116d4c1c9f3d606f7a97224ded999e4
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-07-29 11:25:59 +0700

    u

--- src/app/Controller/AdminAuthController.js
+++ src/app/Controller/AdminAuthController.js
@@ -1,45 +1,18 @@
 class AdminAuthController {
     login(req, res) {
         return res.render('auth/login', {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            layout: false
-=======
-            layout: false,
->>>>>>> 824002c33fca7156164424210b4addd4c4fcf576
-=======
-            layout: false,
-=======
-            layout: false
->>>>>>> 9976f261982f6fd2df5b85cfcaee3acac0ed7665
->>>>>>> c8c0431c4f4942e507af55e1cddb489657edc5a1
+
+
+     
         });
     }
     register(req, res) {
         return res.render('auth/register', {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            layout: false
+
+
+      
         });
     }
 }
 module.exports = new AdminAuthController
-=======
-=======
->>>>>>> c8c0431c4f4942e507af55e1cddb489657edc5a1
-            layout: false,
-        });
-    }
-}
-module.exports = new AdminAuthController();
-<<<<<<< HEAD
->>>>>>> 824002c33fca7156164424210b4addd4c4fcf576
-=======
-=======
-            layout: false
-        });
-    }
-}
-module.exports = new AdminAuthController
->>>>>>> 9976f261982f6fd2df5b85cfcaee3acac0ed7665
->>>>>>> c8c0431c4f4942e507af55e1cddb489657edc5a1
+
